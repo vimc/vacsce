76d018ab40c1d51e64e06a2ba0eb2faa858fdda1
Xiang Li <x.li@imperial.ac.uk>
2023-06-16 11:02:19 +0100

    initiation

---
+++ R/general.R
@@ -0,0 +1,25 @@
+input_check <- function(input){
+  ## input is a list of objects containing
+  ## user - vimc (requires db connection) or external (matches montagu convention)
+  ## country - ISO3 code
+  ## disease - disease name following VIMC convention, currently
+  # c("HepB", "Hib", "HPV", "JE", "Measles", "MenA", "PCV", "Rota",
+  #   "Rubella", "YF", "DTP", "Cholera", "Typhoid", "COVID", "Malaria"
+  # )
+  ## r_int - routine introduction year
+  ## rout_hist - historical routine data, nullable if not applicable or future intro
+  ## sia_hist - historical campaign data, nullable if not applicable or not of interest,
+
+
+  ##todo: duplicate from vimpact/util.R assert_XXX functions and further develop if needed
+
+}
+
+vac_hist <- function(input){
+
+}
+
+vac_proj <- function(){
+
+}
+
