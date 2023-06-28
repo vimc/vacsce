524521aa702f7a64099cd40eeb267c0ebbba0070
Xiang Li <x.li@imperial.ac.uk>
2023-06-28 13:33:48 +0100

    save progress

--- R/general.R
+++ R/general.R
@@ -1,79 +1,64 @@
 #' saninty check for input date
 #' @param input
-#' input is a list of objects. It consists of
-#'  user - vimc (requires db connection) or external (matches montagu convention)
-#'  coverage_src - touchstone for vimc, any narrative for external
-#'  year_cur - current year
-#'  country - ISO3 code
-#'  disease - disease name
-#'  r_int - routine introduction year
-#'  rout_his - historical routine data, nullable if not applicable or future intro
-#'  sia_his - historical campaign data, nullable if not applicable or not of interest,
-#'  rout_fut - historical routine data, nullable if not applicable or future intro
-#'  sia_fut - historical campaign data, nullable if not applicable or not of interest,
-#'  proj_rul - projection rule as a list (keep_levels, incremental, catch_up_with_x, non_linear_scale_up), the combination of them make full projection trajectory
-#'  keep_levels - year_from, year_to, level
-#'  incremental - year_from, year_to, step
-#'  catch_up_with_x - year_from, year_to, vaccine_x # usually for routine introduction
-#'  non_linear_scale_up - year_from, year_to, endpoint
-#'  each rule can be used multiple of times, BUT, rules have to be set-up according to timeline
-#' sainty check for input data
 #' input - input parameters
-#' con - montagu database connection
-input_check <- function(input, con){
-  ## input is a list of objects containing
-  stopifnot(input$user == "vimc" | input$user == "external")
-  stopifnot(length(input$country) == 1L) # work for one country only
-  stopifnot(length(input$disease) == 1L) # work for one disease only
+#' can project deliveries one-by-one if one depends on another, e.g. campaign frequency may be determined by past routine services
+# for each delivery can also work on rules one-by-one if a secondary rule depends on earlier projection, e.g. add a condition, if covid disruption..., if recovered, ...
+# I will provide examples
+input_check <- function(input){
+  ## input is a list of objects
+  ## need meaningful checks to avoid errors
+  country <- unique(input$params$country)
+  disease <- unique(input$params$disease)
+  year_cur <- unique(input$params$year_cur)
+  introduction <- input$params$introduction
+  stopifnot(length(country) == 1L) # work for one country only
+  stopifnot(length(disease) == 1L) # work for one disease only
+  stopifnot(length(year_cur) == 1L) # curent year
 
-  if (input$user == "vimc"){
-    ## log-on to montagu and extract historical coverage
-    ## over-write params r_int, rout_hist and sia_hist
-    touch_his <- vimpact::get_touchstone(con, input$coverage_src_his)
-    touch_fut <- vimpact::get_touchstone(con, input$coverage_src_fut)
+  ## filter data
+  his <-  input$src$historic %>%
+    filter(country == !!country) %>%
+    right_join(introduction %>% select(-year_intro), by = c("vaccine", "activity_type"))
 
-    his <- DBI::dbGetQuery(con,
-                           paste("SELECT country, vaccine, activity_type, year, age_from, age_to, gender, target, coverage, proportion_risk",
-                                 "FROM coverage_set JOIN coverage ON coverage.coverage_set = coverage_set.id",
-                                 "WHERE country = $1",
-                                 sprintf("AND name LIKE %s", paste0("'%", input$disease, "%'")),
-                                 "AND touchstone = $2",
-                                 "AND coverage > 0",
-                                 "AND year <= $3"),
-                           list(input$country,
-                                touch_his,
-                                input$year_cur))
-    fut <- DBI::dbGetQuery(con,
-                           paste("SELECT country, vaccine, activity_type, year, age_from, age_to, gender, target, coverage, proportion_risk",
-                                 "FROM coverage_set JOIN coverage ON coverage.coverage_set = coverage_set.id",
-                                 "WHERE country = $1",
-                                 sprintf("AND name LIKE %s", paste0("'%", input$disease, "%'")),
-                                 "AND touchstone = $2",
-                                 "AND coverage > 0",
-                                 "AND year > $3"),
-                           list(input$country,
-                                touch_fut,
-                                input$year_cur))
+  fut <-  input$src$future %>%
+    filter(country == !!country) %>%
+    right_join(introduction %>% select(-year_intro), by = c("vaccine", "activity_type"))
 
-    input$rout_his <- his[his$activity_type == "routine", ]
-    input$sia_his <- his[his$activity_type == "campaign", ]
-    input$rout_fut <- fut[fut$activity_type == "routine", ]
-    input$sia_fut <- fut[fut$activity_type == "campaign", ]
-    x <-bind_rows(input$rout_his, input$rout$fut)
-    input$rout_int <- x %>% group_by(vaccine, activity_type) %>%
-      summarise(year_intro = min(year), .groups = "keep") %>%
-      as.data.frame()
-    message("Further expand if activity_types other than routine and campaign, e.g. routine_intensified")
+  ## check introduction
+  ## merge data source and user specified introduction dates
+  s <- his %>% group_by(vaccine, activity_type) %>%
+    filter(activity_type != "campaign") %>%
+    summarise(year_intro_db = min(year), .groups = "keep") %>%
+    as.data.frame() %>%
+    right_join(introduction, by = c("vaccine", "activity_type")) %>%
+    mutate(conflict = (!is.na(year_intro) & !is.na(year_intro_db) & year_intro != year_intro_db))
+  if (any(s$conflict)){
+    message("conflict in introduction assumption")
+    message("you may want to act as external user making up hypothetic historical coverage other than actual?")
+    stop(print(s))
   }
+
+  input$introduction <- s %>%
+    mutate(year_intro = ifelse(is.na(year_intro), year_intro_db, year_intro)) %>%
+    select(vaccine, activity_type, year_intro) %>%
+    mutate(future_introduction = !is.na(year_intro) & year_intro > year_cur)
+  input$his <- his %>% right_join(input$introduction %>% filter(!future_introduction), by = join_by("vaccine", "activity_type")) %>%
+    select(-year_intro, - future_introduction)
+  input$fut <- fut # future coverage is used for each delivery if no corresponding projection rule(s) specified
+  message("Further expand if activity_types other than routine and campaign, e.g. routine_intensified")
 
   ## satiny check for rules
   for(j in seq_len(length(input$proj_rul))){
     r <- input$proj_rul[[j]]
     x <- length(r)
-    print(sprintf("%s projection rules identified for %s", x, input$rout_int$vaccine[j]))
+    if(x == 0){
+      message(sprintf("%s projection rules identified for %s %s; use any future coverage specified.", x, input$introduction$activity_type[j], input$introduction$vaccine[j]))
+    } else {
+      message(sprintf("%s projection rules identified for %s %s", x, input$introduction$activity_type[j], input$introduction$vaccine[j]))
+    }
     for (i in seq_len(x)){
-      print(paste("rule", i, "-",names(r[i])))
-      print(as.data.frame(r[[i]]))
+      # print(paste("rule", i, "-",names(r[i])))
+      # print(as.data.frame(r[[i]]))
       stopifnot(r[[i]]$year_from <= r[[i]]$year_to)
       if(i < x){
         if(r[[i]]$year_to+1 != r[[i+1]]$year_from){
@@ -82,48 +67,66 @@ input_check <- function(input, con){
       }
     }
   }
-
-
   return(input)
 }
 
 vac_sce <- function(input){
-  x <- nrow(input$rout_int)
+  his <- input$his
+  fut <- input$fut
 
+  x <- nrow(input$introduction)
+
+  dat <- list(NULL)
   for(i in seq_len(x)){
-    # for each routine vaccine do scenario projection
-    v <- input$rout_int$vaccine[i] # vaccine name
-    print(sprintf("projecting trajectory for vaccine %s", v))
-
+    # for each vaccine delivery do scenario projection
+    print(sprintf("projecting trajectory for %s %s", input$introduction$activity_type[i], input$introduction$vaccine[i]))
+    d0 <- his %>%
+      right_join(input$introduction[i, c("vaccine", "activity_type")], by = c("vaccine", "activity_type") )
+    d1 <- fut %>%
+      right_join(input$introduction[i, c("vaccine", "activity_type")], by = c("vaccine", "activity_type") )
+    d <- d0 %>%
+      select(year, coverage, age_from, age_to) %>%
+      filter(!is.na(year)) %>%
+      arrange(year)
     r <- input$proj_rul[[i]] # projection rules
-    for(j in seq_len(length(r))){
-      func <- paste0(names(r[j]), sub("list", "",paste(r[j])))
-      print(func)
-      #eval(parse(text = func))
-    }
 
+    if(!is.null(r)){
+      for(j in seq_len(length(r))){
+        func <- paste0(names(r[j]), sub("list\\(", "(d, ",paste(r[j])))
+        print(func)
+        d <- eval(parse(text = func))
+      }
+      dat[[i]] <- d %>% bind_cols(input$introduction[i, c("vaccine", "activity_type")])
+    } else{
+      print("No projection. Binding data from source coverage.")
+      dat[[i]] <- bind_rows(d0, d1)
+    }
   }
+  dat <- do.call(bind_rows, dat) %>%
+    mutate(country = input$params$country,
+           disease = input$params$disease)
 }
 
 ## projection rules
-## keep at certain coverage level
+## rule a.) keep at certain coverage level
+## d is a data frame containing at least (year, coverage)
 keep_levels <- function(d, year_from, year_to, level){
   t <- d[1, ] %>%
     select(-year) %>%
-    right_join(data.frame(year = year_from:year_to)) %>%
+    merge(data.frame(year = year_from:year_to)) %>%
     mutate(coverage = level) %>%
     bind_rows(d) %>%
     arrange(year)
   return(t)
 }
 
-## equal-step increase
-## d is a dataframe  (year, coverage)
+## rule b.) equal-step increase
+## d is a data frame containing at least (year, coverage)
 incremental <- function(d, year_from, year_to, step, cap = 0.95){
   y <- max(d$year)
   t <- d[d$year == y, ] %>%
     select(-year) %>%
-    right_join(data.frame(year = year_from:year_to)) %>%
+    merge(data.frame(year = year_from:year_to)) %>%
     mutate(coverage = coverage + step*(year - y)) %>%
     bind_rows(d) %>%
     arrange(year) %>%
@@ -131,31 +134,32 @@ incremental <- function(d, year_from, year_to, step, cap = 0.95){
   return(t)
 }
 
-## this is specifically for routine intro, but can be used flexibly
-## d is a dataframe  (year, coverage)
+## rule c.) this is specifically for routine intro, but can be used flexibly
+## d is a data frame containing at least (year, coverage)
 catch_up_with_x <- function(d, year_from, year_to, vaccine_x_level, intro_level = 1/3){
   years <- seq(year_from, year_to, 1)
   cov <- rep(NA, length(years))
   if (nrow(d) == 0){
     ## routine introduction
     cov[1] <- vaccine_x_level*intro_level # intro at 33% of target vaccine
-    cov[-1] <- IA2030_projection(year_from, covs[1], year_to, vaccine_x_level)
+    cov[-1] <- IA2030_projection(year_from, cov[1], year_to, vaccine_x_level)
   } else {
     ## non-lenear scale-up
     message("intro_level is not used as not applicable")
-    cov <- IA2030_projection(year_from, d[d$year == year_from -1], year_to, vaccine_x_level)
+    cov <- IA2030_projection(year_from-1, d[d$year == year_from -1], year_to, vaccine_x_level)
   }
   dat <- bind_rows(d, data.frame(year = years, coverage = cov))
   return(dat)
 }
 
-## ia2030 non-linear scale-up function
+## rule d.) ia2030 non-linear scale-up function
+## d is a data frame containing at least (year, coverage)
 non_linear_scale_up <- function(d, year_from, year_to, endpoint){
-  cov <- IA2030_projection(year_from, d[d$year == year_from -1], year_to, endpoint)
+  years <- seq(year_from, year_to, 1)
+  cov <- IA2030_projection(year_from-1, d$coverage[d$year == year_from -1], year_to, endpoint)
   dat <- bind_rows(d, data.frame(year = years, coverage = cov))
   return(dat)
 }
-
 
 ## ia2030 non-linear function
 IA2030_projection <- function(t0, c0, T, cT){
@@ -174,7 +178,33 @@ IA2030_projection <- function(t0, c0, T, cT){
   }
 }
 
+## extract source coverage for vimc user
+vimc_historical_data <- function(con, year_cur, coverage_src_his, coverage_src_fut){
+  touch_his <- vimpact::get_touchstone(con, coverage_src_his)
+  touch_fut <- vimpact::get_touchstone(con, coverage_src_fut)
+
+  # extract data from montagu
+  his <- DBI::dbGetQuery(con,
+                         paste("SELECT country, vaccine, activity_type, year, age_from, age_to, gender, target, coverage, proportion_risk",
+                               "FROM coverage_set JOIN coverage ON coverage.coverage_set = coverage_set.id",
+                               "WHERE touchstone = $1",
+                               "AND coverage > 0",
+                               "AND year <= $2"),
+                         list(touch_his,
+                              year_cur))
+  fut <- DBI::dbGetQuery(con,
+                         paste("SELECT country, vaccine, activity_type, year, age_from, age_to, gender, target, coverage, proportion_risk",
+                               "FROM coverage_set JOIN coverage ON coverage.coverage_set = coverage_set.id",
+                               "WHERE touchstone = $1",
+                               "AND coverage > 0",
+                               "AND year > $2"),
+                         list(touch_fut,
+                              year_cur))
+  return(list(historic = his,
+              future = fut))
+}
 
 ## sanity checks
 ## what to check?
 ## continuity, range, trend, pop, age, etc
+
