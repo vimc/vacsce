c1b09edc67015db362fc8053d572bde7553ccd16
Xiang Li <x.li@imperial.ac.uk>
2023-07-14 09:56:04 +0100

    upload

---
+++ R/util.R
@@ -0,0 +1,10 @@
+assert_has_columns <- function(data, col_names,
+                               name = deparse(substitute(data))) {
+  missing_names <- setdiff(col_names, colnames(data))
+  if (length(missing_names) > 0) {
+    stop(sprintf("Required column names %s are missing from %s",
+                 paste(missing_names, collapse = ", "),
+                 name))
+  }
+  invisible(TRUE)
+}
