196f7ad8342743fac3dc821e75db8025f3e23192
Xiang Li <x.li@imperial.ac.uk>
2023-10-10 17:43:32 +0100

    updates

--- R/zzz.R
+++ R/zzz.R
@@ -3,6 +3,6 @@
 ##' @importFrom stats runif
 ##' @importFrom rlang .data
 utils::globalVariables(c("activity_type", "age_from", "age_to", "conflict", "coverage", "future_introduction",
-                         "gender", "vaccine", "year", "year_intro", "year_intro_src"))
+                         "gender", "vaccine", "year", "year_intro", "year_intro_src", "region"))
 NULL
 
